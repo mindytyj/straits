f471f83422d4976b5f117dbfc85fa983a7287b9f
mindytyj <derpbritz@gmail.com>
2024-01-31 00:30:28 +0800

    DB Configuration Set Up

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
+# import os
+# from dotenv import load_dotenv
+# import mysql.connector
+#
+# load_dotenv(".env")
+#
 app = Flask(__name__)
+#
+# # Database Configuration
+# connection = mysql.connector.connect(
+#     user=os.getenv('DB_USER'),
+#     password=os.getenv('DB_PASSWORD'),
+#     host=os.getenv('DB_HOST'),
+#     port=os.getenv('DB_PORT'),
+#     database=os.getenv('DB_NAME')
+# )
 
-
+# Routes
 @app.route("/")
 def login():
+    # dbquery = connection.cursor()
+    # dbquery.execute("SQL QUERY HERE")
+    # dbquery.close()
+    # connection.close()
     return "Login Page"
 
 @app.route("/hr")
