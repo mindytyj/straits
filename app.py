09cf85b2c9682cf05d20af64a3790bcb57fb2669
mindytyj <derpbritz@gmail.com>
2024-02-05 22:32:12 +0800

    Update Routes and Include Adding and Deletion of Courses

--- app.py
+++ app.py
@@ -32,9 +32,27 @@ def submit():
     else:
         return render_template("login.html")
 
-@app.route("/hr")
-def hr():
-    return "HR Page"
+@app.route("/courses")
+def courses():
+    dbquery = database.connection.cursor(dictionary=True)
+    dbquery.execute("SELECT t1.CourseID, t1.CourseName from Courses t1 LEFT JOIN Training t2 on t1.CourseID = t2.CourseID LEFT JOIN Users t3 on t2.UserID = t3.UserID WHERE t2.UserID = 3;")
+    data = dbquery.fetchall()
+    dbquery.close()
+    return render_template("courses.html", courses=data)
+
+@app.route("/courses/<string:CourseID>/add", methods=['GET', 'POST'])
+def addcourse(CourseID):
+    dbquery = database.connection.cursor(dictionary=True)
+    dbquery.execute("INSERT INTO Training (UserID, CourseID) VALUES (3, %s)", (CourseID,))
+    dbquery.close()
+    return redirect(url_for('courses'))
+
+@app.route("/courses/<string:CourseID>/delete", methods=['GET', 'POST'])
+def deletecourse(CourseID):
+    dbquery = database.connection.cursor(dictionary=True)
+    dbquery.execute("DELETE FROM Training WHERE UserID = 3 AND CourseID = %s", (CourseID,))
+    dbquery.close()
+    return redirect(url_for('courses'))
 
 if __name__ == "__main__":
     app.run()
